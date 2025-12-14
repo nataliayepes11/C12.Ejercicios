8096c525e8e2314681f7f4893016ec918f622677
nataliayepes11 <nataliayepes11@gmail.com>
2025-12-14 16:41:30 -0300

    CICLO FOR - ARRAY

--- Ej4.js
+++ Ej4.js
@@ -1,11 +1,22 @@
-/* Ejercicio 4: Ciclo For - Array
-Crear un programa que permita registrar las notas de varios estudiantes usando arrays y mostrarlas por pantalla:
-1. Crear un array para almacenar las notas.
-2. Defi ne un array vacío llamado notas donde almacenaremos las notas de los estudiantes.
-3. Pide al usuario que ingrese las notas de varios estudiantes una por una utilizando el método prompt.
-4. Usa un bucle for para solicitar las notas y asignarlas directamente a posiciones específi cas del array notas.
-5. Imprime en consola las notas ingresadas usando el array notas. 
-*/
+// Ejercicio 4: Ciclo For - Array
+//Crear un programa que permita registrar las notas de varios estudiantes usando arrays y mostrarlas por pantalla:
+
 
 const prompt = require('prompt-sync')();
 
+// 1. Crear un array para almacenar las notas.
+let notas = [];
+
+// 2. Define un array vacío llamado notas donde almacenaremos las notas de los estudiantes.
+let totalEstudiantes = 11; 
+// titulito bonito
+console.log("Veamos las notas de tus " + totalEstudiantes + " estudiantes.");
+// 4. Usa un bucle for para solicitar las notas y asignarlas directamente a posiciones específicas del array notas.
+for (let i = 0; i < totalEstudiantes; i++) {
+// 3. Pide al usuario que ingrese las notas de varios estudiantes una por una utilizando el método prompt    
+  notas[i] = parseFloat(prompt("Ingresá la nota del estudiante " + (i + 1) + ": "));
+}
+// titulito bonito
+console.log("--- Notas Ingresadas ---");
+// 5. Imprime en consola las notas ingresadas usando el array notas. 
+console.log(notas);
