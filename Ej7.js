0f395f5bf6fca640281c28dd669a89d3e0b47585
nataliayepes11 <nataliayepes11@gmail.com>
2025-12-16 12:39:30 -0300

    MATRIZ

--- Ej7.js
+++ Ej7.js
@@ -3,4 +3,20 @@ Declara una variable que contenga una matriz de 5x5 llena de puros números ente
 Luego, escribe un algoritmo para sumar todos los números en la matriz.
 */ 
 
-const prompt = require('prompt-sync')();
+let matriz = [
+[2, 5, 8, 45, 69],
+[15, 58, 29, 78, 36],
+[2, 4, 6, 8, 10],
+[11, 33, 55, 77, 99],
+[22, 44, 66, 88, 1]
+];
+
+let suma = 0;
+
+for (let i = 0; i < matriz.length; i++) {
+    for (let j = 0; j < matriz[i].length; j++) {
+        suma += matriz[i][j];
+    }
+}
+
+console.log("Todos los números de la matríz suman: " + suma);
