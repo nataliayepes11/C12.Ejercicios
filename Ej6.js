93f178d3f24c51faaeb7fa46315a4e2b884fa817
nataliayepes11 <nataliayepes11@gmail.com>
2025-12-16 12:23:42 -0300

    MATRIZ

--- Ej6.js
+++ Ej6.js
@@ -3,4 +3,22 @@ Dada una matriz, recorrer sus valores y sumar solo los números que estén por e
 VER MATRIZ EN LA HOJA
 */ 
 
-const prompt = require('prompt-sync')();
+let matriz = [
+    [10, 3, 2, 1, 4, 7],
+    [5, 5, 10, 1000, 4],
+    [5, 125, 10, 1020, 4],
+    [5, 5, 5097, 100, 4]
+    ];
+
+let sumaCondicional = 0;
+   for (let i =0; i < matriz.length; i++) {
+   for (let j =0; j < matriz.length; j++) {
+let valorActual = matriz[i][j];
+   if (valorActual >= 10 && valorActual < 1000) {
+
+   sumaCondicional += valorActual;
+}
+}
+}    
+
+console.log("La suma de los valores >= 10 y < 1000 es: " + sumaCondicional);
